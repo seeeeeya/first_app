5619687a6871ab4cb773dfb117fcea5346af4fbd
seiya <74032023+seeeeeya@users.noreply.github.com>
2020-11-06 12:29:00 +0900

    first commit

---
+++ app/assets/config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  # get 'posts', to: 'posts#index'
+  # get 'posts/new', to: 'posts#new'
+  
+    get 'posts', to: 'posts#index'
+    get 'posts/new', to: 'posts#new'
+
+end
